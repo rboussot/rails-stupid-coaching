e78c0df04700d9aabddb86dd020e12ade726b515
Romain Boussot <rboussot@gmail.com>
2016-08-08 12:59:54 +0200

    stupid coach enhenced

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,12 +1,15 @@
 class CoachingController < ApplicationController
   def answer
     @query = params[:query]
-    if @query == "I am going to work right now!"
-      exit
-    elsif @query == @query.upcase
-      @answer = "I can feel your motivation!"
+    @insults = ["merde", "con", "salaud", "connard", "gueule", "nique"]
+    if @query.include?("go") || @query.include?("Go")
+      @answer = "OK!"
+    elsif @insults.include?(@query)
+      @answer = "Manque de respect = 100 pompes !"
     elsif @query.include?("?")
       @answer = "Silly question, get dressed and go to work!"
+    elsif @query.include?("!") || @query == @query.upcase
+      @answer = "I can feel your motivation!"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'answer', to: 'coaching#answer'
-  get 'ask', to: 'coaching#ask'
+  get 'ask', to: 'coaching#ask', as: :question
   root to: 'coaching#ask'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
